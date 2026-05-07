8def522d7230781583616b6a0b046d2b6fb49450
Vinicius Araujo Santos <v.a.s.97@hotmail.com>
2026-05-06 21:02:01 -0300

    correções de pastas

--- Gerenciador/criar_banco.py
+++
@@ -1,6 +0,0 @@
-from projetosenac import app, database
-
-# Roda isso UMA vez para criar o banco de dados
-with app.app_context():
-    database.create_all()
-    print("Banco de dados criado com sucesso!")
